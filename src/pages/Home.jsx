82a941d097480f3aaab387dee454b6b3cf46340c
Aloysius <zerolimits45@gmail.com>
2024-12-22 20:10:21 +0800

    testin till i lose it

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,8 @@
-import { useContext, useEffect, useState } from 'react'
+import { useContext, useEffect, useState, Suspense, useRef, useLayoutEffect } from 'react'
 import { Route, Routes, Navigate, Link } from 'react-router-dom'
 //import NotFound from './errors/NotFound'
 //import { UserContext } from '..'
-import { Button, Container, Divider, Typography, Box, Card, TextField, Skeleton, CardContent, CardMedia, Chip, Alert, Collapse, Grid } from '@mui/material'
+import { Button, Container, Divider, Typography, Box, Card, TextField, Skeleton, CardContent, CardMedia, Chip, Alert, Collapse, Grid, Stack} from '@mui/material'
 import { AppContext } from '../App';
 import { HomeRounded, LoginRounded, NewReleasesRounded, SearchRounded, WarningRounded } from '@mui/icons-material';
 import titleHelper from '../functions/helpers';
@@ -10,6 +10,10 @@ import http from '../http';
 import { useSnackbar } from "notistack";
 import moment from 'moment';
 import { get } from 'aws-amplify/api';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Environment } from '@react-three/drei';
+import Cloud from '../../public/Cloud'
+import { gsap } from 'gsap';
 
 
 function Home() {
@@ -65,6 +69,24 @@ function Home() {
         )
     }
 
+    const comp = useRef(null);
+    const textRef = useRef(null);
+    const canvasRef = useRef(null);
+    const skyRef = useRef(null);
+    const charRef1 = useRef([]);
+    const charRef2 = useRef([]);
+
+    useLayoutEffect(() => {
+        let ctx = gsap.context(() => {
+            const tl = gsap.timeline()
+            tl.from(charRef1.current, { yPercent: -500, duration: 2, delay: 2, stagger: 0.5, ease: "back.out" })
+            tl.from(canvasRef.current, { xPercent: 1000, duration: 2, delay: 0 })
+            tl.from(charRef2.current, { xPercent: 700, opacity: 0, duration: 2, delay: 0, stagger: 0.5, ease: "bounce.inOut" })
+        }, comp)
+
+        return () => ctx.revert();
+    }, [])
+
     useEffect(() => {
         //getBanners()
         //getActivities()
@@ -72,20 +94,44 @@ function Home() {
 
     return (
         <>
-            <Container maxWidth="false" sx={{ backgroundColor: "#B2CC83", height: "100vh" }}>
-                <Grid container spacing={2}>
-                    <Grid item xs={12} md={6}>
-                        <Box sx={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", height: "100%" }}>
-                            <Typography variant="h2" sx={{ color: "#fff" }}>Welcome to the</Typography>
-                            <Typography variant="h2" sx={{ color: "#fff" }}>Spline Tool</Typography>
-                        </Box>
-                    </Grid>
-                    <Grid item xs={12} md={6}>
-                        <Box sx={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", height: "100%" }}>
-                        \
-                        </Box>
-                    </Grid>
-                </Grid>
+            <Container maxWidth="false" sx={{ backgroundColor: "#B2CC83", height: "100vh" }} ref={comp}>
+                <Canvas ref={canvasRef}>
+                    <ambientLight />
+                    <OrbitControls enableZoom={false} enableRotate={false} />
+                    <Suspense fallback={null}>
+                        <Cloud position={[8, 1, -5]} rotation={[0, Math.PI / 6, 0]} />
+                    </Suspense>
+                    <Environment preset="sunset" />
+                </Canvas>
+                <Box
+                    sx={{
+                        position: "absolute",
+                        top: "50%",
+                        left: "30%",
+                        transform: "translate(-50%, -50%)",
+                        textAlign: "center",
+                        zIndex: 10, // Ensure the text is above the Canvas
+                        color: "white",
+                    }}
+                >
+                    <Stack direction={"row"}>
+                        <Typography ref={textRef} variant='h1' style={{ fontFamily: "'Poppins', sans-serif", fontWeight: "700", color: "#44624A" }}>
+                            {"Midori".split("").map((char, index) => (
+                                <span key={index} ref={el => charRef1.current[index] = el} style={{ display: 'inline-block' }}>
+                                    {char}
+                                </span>
+                            ))}
+                        </Typography>
+
+                        <Typography ref={skyRef} variant='h1' style={{ fontFamily: "'Poppins', sans-serif", fontWeight: "700", color: "White" }}>
+                            {"SKY".split("").map((char, index) => (
+                                <span key={index} ref={el => charRef2.current[index] = el} style={{ display: 'inline-block' }}>
+                                    {char}
+                                </span>
+                            ))}
+                        </Typography>
+                    </Stack>
+                </Box>
             </Container>
         </>
     )
