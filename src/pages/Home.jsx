18eef10c9be02b1b40ab53e8ad6ac45baff4c382
Joseph Lee <facebooklee52@gmail.com>
2025-02-18 14:58:26 +0800

    Change background to glb

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -116,7 +116,6 @@ function Home() {
     useEffect(() => {
         //getBanners()
         //getActivities()
-
     }, [])
 
     // ----------------- THREE.JS Scene Component -----------------
@@ -168,7 +167,7 @@ function Home() {
             controls.enableDamping = true;
             controls.dampingFactor = 0.05;
 
-            // Load 3D model
+            // Load 3D model (now loading a .glb file)
             const loader = new GLTFLoader();
             let mixer;
             loader.load(
@@ -243,10 +242,10 @@ function Home() {
         return <div ref={mountRef} style={{ width: '100%', height: '100%', position: 'relative' }} />;
     });
 
-    // Components for different scenes
+    // Components for different scenes with .glb files
     const BackgroundScene = () => {
          return <ThreeScene
-            modelPath="./background.gltf"
+            modelPath="./background.glb"
             position={[5, 1, -5]}
             scale={[0.6, 0.6, 0.6]}
             rotation={[0, Math.PI / 2, 0]}
@@ -255,7 +254,7 @@ function Home() {
 
     const LeafScene = () => (
         <ThreeScene
-            modelPath="./Leaf.gltf"
+            modelPath="./Leaf.glb"
             position={[0, 0, -23]}
             scale={[1, 1, 1]}
             rotation={[0, Math.PI / 2, 0]}
